356734eeff06ef195320410a275efba8fda8972a
RBoraurito <r.boraure@kation.com.do>
2021-10-09 21:54:04 -0400

    feat: :sparkles: Add get categories endpoints

--- api/categories.js
+++ api/categories.js
@@ -5,15 +5,16 @@ const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 })
 
-async function getList() {
+async function getCategories() {
   const databaseId = 'cbd8aa4912484612850172850d093308';
   const response = await notion.databases.retrieve({ database_id: databaseId });
-  return response
+  const categories = response.properties.Categories.select.options.map(el => el.name)
+  return categories
 }
 
 module.exports = async (req, res) => { // this function will be launched when the API is called.
   try {
-    res.send(await getList()) // send the lyrics
+    res.send(await getCategories()) // send the lyrics
   } catch (err) {
     res.send(err) // send the thrown error
   }
