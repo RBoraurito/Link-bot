62cc4bd57896f5fad59a5da0e783a29488ac6d50
RBoraurito <r.boraure@kation.com.do>
2021-10-09 21:05:14 -0400

    feat: :sparkles: Add response test

--- api/index.js
+++ api/index.js
@@ -4,3 +4,11 @@ const { Client } = require("@notionhq/client")
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 })
+
+module.exports = async (req, res) => { // this function will be launched when the API is called.
+  try {
+    res.send('Test') // send the lyrics
+  } catch (err) {
+    res.send(err) // send the thrown error
+  }
+}
