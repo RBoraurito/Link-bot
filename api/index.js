e975ddbf61414c78dc47a99369b70e867ba7f9d5
RBoraurito <r.boraure@kation.com.do>
2021-10-10 22:00:51 -0400

    test

--- api/index.js
+++ api/index.js
@@ -5,22 +5,13 @@ const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 })
 
-async function debug() {
-  // const databaseId = 'cbd8aa4912484612850172850d093308';
-  // const response = await notion.databases.query({ database_id: databaseId });
-
-  // const links = response.results
-  // .map(el => {
-  //   const name = el.properties.Name.title[0].plain_text || ''
-  //   const link = el.properties.Link.url || ''
-  //   return `${name} - ${link}`
-  // })
-  // return links
+function debug(req) {
+  return req.body
 }
 
 module.exports = async (req, res) => { // this function will be launched when the API is called.
   try {
-    res.send(req) // send the lyrics
+    res.send(debug(req)) // send the lyrics
   } catch (err) {
     res.send(err) // send the thrown error
   }
